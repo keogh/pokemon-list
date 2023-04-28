b687edf26e94adcc1b8c6e5da1c4f410fc3c0c66
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2023-04-28 07:47:11 -0700

    Add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { useQuery , gql} from '@apollo/client';
 import PokemonsList from "./Components/PokemonsList";
+import Pagination from "./Components/Pagination/Pagination";
+import {useLocation} from "react-router-dom";
 
 const GET_POKEMONS = gql`
   query pokemons($limit: Int, $offset: Int) {
@@ -22,19 +24,31 @@ const GET_POKEMONS = gql`
 `;
 
 function App() {
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const page = parseInt(query.get('page') || '1', 10);
+  const itemsPerPage = 24;
+  const offset = itemsPerPage * (page - 1);
+
+  // TODO: maybe use router loader
   const { loading, error, data } = useQuery(GET_POKEMONS, {
     variables: {
-      limit: 12,
-      offset: 0,
+      limit: itemsPerPage,
+      offset: offset,
     },
   });
 
   if (loading) return <>'Loading...'</>;
   if (error) return <>`Error! ${error.message}`</>;
 
+  const numPages = Math.ceil(data.pokemons.count / itemsPerPage);
+
 
   return (
-    <PokemonsList pokemons={data.pokemons.results} />
+    <>
+      <PokemonsList pokemons={data.pokemons.results} />
+      <Pagination page={page} count={numPages} />
+    </>
   );
 }
 
---
+++ src/Components/Pagination/Pagination.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {Link} from "react-router-dom";
+import { Pagination as MUIPagination, PaginationItem } from "@mui/material";
+
+
+interface Props {
+  page: number,
+  count: number,
+}
+
+export default function Pagination({ page, count }: Props) {
+  return (
+    <MUIPagination
+      page={page}
+      count={count}
+      renderItem={(item) => (
+        <PaginationItem
+          component={Link}
+          to={`/${item.page === 1 ? '' : `?page=${item.page}`}`}
+          {...item}
+        />
+      )}
+    />
+  );
+}
